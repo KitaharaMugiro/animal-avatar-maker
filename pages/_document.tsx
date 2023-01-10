b27a729589696df39afe4f4cc20e3e36fa677fab
KitaharaMugiro <you@example.com>
2023-01-10 17:21:57 +0900

    重いの気になる

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Animal avatar maker"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="アニマルアバターメーカー" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="アニマルアバターメーカー"
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="アニマルアバターメーカー" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="アニマルアバターメーカー" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="アニマルアバターメーカー"
           />
         </Head>
         <body className="bg-black antialiased">

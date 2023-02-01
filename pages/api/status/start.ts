aaf86f894c573dd7aad4fc2b429e02a576c41597
KitaharaMugiro <you@example.com>
2023-02-01 20:53:07 +0900

    error消し込み

--- pages/api/status/start.ts
+++ pages/api/status/start.ts
@@ -5,23 +5,9 @@ import { hasuraRequest } from "../../../utils/hasura";
 export default async (req, res) => {
     const { user_id, class_name, prompts } = req.body;
     const query = gql`
-            mutation ($user_id: String, $plan: String, $prompt_version: numeric, $class_name: String) {
-            insert_avatar_generate_status_one(
-                object: {
-                user_id: $user_id, 
-                plan: $plan, 
-                prompt_version: $prompt_version,
-                class_name : $class_name,
-                status: "waiting"
-                
-                }, on_conflict: {
-                constraint: avatar_generate_status_pkey, 
-                update_columns: [plan, prompt_version, class_name, status]}) {
-                user_id
-            }
-        }
+            
     `
-    const variables = { "user_id": user_id, "plan": plan, "prompt_version": prompt_version, "class_name": class_name }
+    const variables = { "user_id": user_id, "class_name": class_name }
 
     const hasuraResponse = await hasuraRequest(query, variables)
     console.log(hasuraResponse)

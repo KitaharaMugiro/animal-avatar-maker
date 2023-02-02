81779c984c4f11fc158f2218f5b72165c1eebeea
sukini0314 <sukini0314@gmail.com>
2023-02-02 23:43:00 +0900

    “コミット”

--- discord.ts
+++ discord.ts
@@ -22,7 +22,6 @@ export default async (req, res) => {
         // const json = await response.json();
         console.log(response.status);
     }
-    
     await main();
     res.status(200).json({
         message: "ok"

348522b6576ab003138c8f4185ae60646601c285
sukini0314 <sukini0314@gmail.com>
2023-02-02 23:29:00 +0900

    “コミット”

---
+++ discord.ts
@@ -0,0 +1,31 @@
+export default async (req, res) => {
+    // discordにwebhookを送信して、「リクエストが送られました」というメッセージを送る
+    // localhost:3000/api/test/discord にフロントエンドからリクエストを送ってDiscordにメッセージが送られるか確認する
+    'use strict'
+
+    const URL = `https://discord.com/api/webhooks/1070330527443329074/KSEhjGiAwWTWa0VCVXokGFjzqcobVb4Oa3ZvuZgRdMqFm4AkxBzJHiMJ1K1zIhskbSsO`;
+    const postData = {
+        username: 'animal_avatar_maker',
+        content: 'Waitingリストに追加されました。'
+    }
+
+    const main = async () => {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData)
+        });
+    
+        // const json = await response.json();
+        console.log(response.status);
+    }
+    await main();
+    res.status(200).json({
+        message: "ok"
+    })
+};
+
+

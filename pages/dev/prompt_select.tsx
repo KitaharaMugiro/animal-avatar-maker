3b15fddcdb4812efd668d5325ddd3ee27d77b131
KitaharaMugiro <you@example.com>
2023-02-09 21:48:37 +0900

    ちょっと改善

--- pages/dev/prompt_select.tsx
+++ pages/dev/prompt_select.tsx
@@ -9,12 +9,12 @@ import Image from 'next/image'
 // STEP3: STEP1で作ったビューの中の１つをクリックすると、それに対応したプロンプトがテキストボックに入力されるようにする
 
 export default () => {
-    let lists =[
-        {avatar:"アニメ風イラスト",image:"/prompts/prompt_4.png",prompt:"{identifier}, beautiful concept art, by makoto shinkai"},
-        {avatar:"バレンタイン",image:"/prompts/prompt_3.png",prompt:"{identifier} with heart chocolate, in valentine"},
-        {avatar:"雪だるま",image:"/prompts/prompt_6.png",prompt:"{identifier} with snowman in snow day"},
-        {avatar:"油絵",image:"/prompts/prompt_2.png",prompt:"neoclassicism {identifier} by Edgar Degas"},
-        {avatar:"アルバム",image:"/prompts/prompt_5.png",prompt:"{identifier} with snowman in album in anime style by makoto shinkai"}
+    let lists = [
+        { avatar: "アニメ風イラスト", image: "/prompts/prompt_4.png", prompt: "{identifier}, beautiful concept art, by makoto shinkai" },
+        { avatar: "バレンタイン", image: "/prompts/prompt_3.png", prompt: "{identifier} with heart chocolate, in valentine" },
+        { avatar: "雪だるま", image: "/prompts/prompt_6.png", prompt: "{identifier} with snowman in snow day" },
+        { avatar: "油絵", image: "/prompts/prompt_2.png", prompt: "neoclassicism {identifier} by Edgar Degas" },
+        { avatar: "アルバム", image: "/prompts/prompt_5.png", prompt: "{identifier} with snowman in album in anime style by makoto shinkai" }
     ]
     const [state, setState] = useState("");
     const [placeholder, setPlaceholder] = useState("ここにプロンプトが表示されます");
@@ -33,20 +33,29 @@ export default () => {
     }
 
     return <div >
-        <p className="mt-1 flex justify-center px-6"><br />プロンプトを選ぶ</p>
-        <div className="mt-1 flex flex-wrap justify-center rounded-md px-6 pt-5 pb-6">
+        <p className="mt-1 flex justify-center px-6">プロンプトを選ぶ①</p>
+        <div className="mt-3 flex flex-wrap justify-center bg-slate-100 p-3">
             {lists.map((val) =>
-            <div className="mx-3 ">
-                <div className="mx-3 cursor-pointer" onClick={addText} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} data-prompt={val.prompt}>
-                <div className="w-1/1"><img className="rounded-xl hover:shadow-2xl" src={val.image} width={200} height={200} alt={val.prompt} />
-                    <p className="mt-1 flex justify-center">{val.avatar}</p></div>
+                <div className="m-1" key={val.prompt}>
+                    <div className="mx-3 cursor-pointer" onClick={addText} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} data-prompt={val.prompt}>
+                        <div className="w-1/1">
+                            <img className="rounded-xl hover:shadow-2xl"
+                                src={val.image} width={100} height={100} alt={val.prompt} />
+                            <p className="mt-1 flex justify-center">{val.avatar}</p></div>
+                    </div>
                 </div>
-            </div>
             )}
-            
+
         </div>
         <div className="mt-1 flex justify-center">
-            <textarea className="focus:shadow-soft-primary-outline min-h-unset text-sm leading-5.6 ease-soft block appearance-none rounded-lg border divide-x border-4 border-purple-300 bg-white bg-clip-padding px-10 py-10 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none" name="" id="" placeholder={placeholder} value={state} />
-        </div><br /><br />
+
+
+            <textarea
+                value={state}
+                placeholder={placeholder}
+                onChange={(e) => setState(e.target.value)}
+                id="message" html-rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+
+        </div>
     </div>
 }

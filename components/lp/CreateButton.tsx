36dc4d4fc9cd9d69f518c1cb5f8d49f469cb7dca
sukini0314 <sukini0314@gmail.com>
2023-03-19 01:13:18 +0900

    commit

--- components/lp/CreateButton.tsx
+++ components/lp/CreateButton.tsx
@@ -4,67 +4,6 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Link from "next/link"
 
-const useStyles = createStyles((theme) => ({
-    hero: {
-        position: 'relative',
-        filter: 'brightness(150%)',
-        backgroundImage:
-            'url(https://res.cloudinary.com/ddeqwb08j/image/upload/v1678450906/19_1_1_oqi6kp.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    },
-
-    container: {
-        height: 500,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-start',
-        paddingBottom: theme.spacing.xl * 6,
-        zIndex: 1,
-        position: 'relative',
-
-        [theme.fn.smallerThan('sm')]: {
-            height: 500,
-            paddingBottom: theme.spacing.xl * 3,
-        },
-    },
-
-    title: {
-        color: theme.white,
-        fontSize: 60,
-        fontWeight: 900,
-        lineHeight: 1.1,
-
-        [theme.fn.smallerThan('sm')]: {
-            fontSize: 40,
-            lineHeight: 1.2,
-        },
-
-        [theme.fn.smallerThan('xs')]: {
-            fontSize: 28,
-            lineHeight: 1.3,
-        },
-    },
-
-    description: {
-        color: theme.white,
-        maxWidth: 600,
-
-        [theme.fn.smallerThan('sm')]: {
-            maxWidth: '100%',
-            fontSize: theme.fontSizes.sm,
-        },
-    },
-
-    control: {
-        marginTop: theme.spacing.xl * 1.5,
-        [theme.fn.smallerThan('sm')]: {
-            width: '100%',
-        },
-    },
-}));
-
 export function CreateButton() {
     return (
         <div className='w-3/5 mx-auto'>

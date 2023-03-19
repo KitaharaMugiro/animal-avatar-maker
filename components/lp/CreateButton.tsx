6fa4fba201386c2244a7778bb315a04a5d4dca74
sukini0314 <sukini0314@gmail.com>
2023-03-19 13:16:58 +0900

    commit

--- components/lp/CreateButton.tsx
+++ components/lp/CreateButton.tsx
@@ -9,7 +9,7 @@ export function CreateButton() {
         <div className='w-3/5 mx-auto'>
             <div className="flex flex-col items-center">
                 <Link href="/top">
-                    <button className="w-full text-xs h-10 md:h-25 my-10 rounded-full bg-red-500 px-10 md:px-20  xl:px-32 xl:text-2xl font-bold text-white hover:bg-red-700">
+                    <button className="w-full text-xs h-10 md:h-16 xl:h-20 my-10 rounded-full bg-red-500 px-10 md:px-20  xl:px-32 xl:text-2xl font-bold text-white hover:bg-red-700">
                         無料で作ってみる
                     </button>
                 </Link>
